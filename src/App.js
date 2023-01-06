511ce407ee6b256044ac3f9b9cd7e6288a9e3674
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2023-01-06 15:45:09 +0500

    add Components i.e Todos,Footer and TodoItems

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import Header from "./Components/Header";
+import Todos from "./Components/Todos";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Todos />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Todos.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Todos = () => {
+  return <div>Todos works</div>;
+};
+
+export default Todos;
